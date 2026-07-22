27b35870ae9a089cc6ae497e32e3bcd534ccaa6c
Ian Fumagalli <ianandre.fumagallimuixi@uzh.ch>
2026-07-22 20:16:13 +0200

    Add list command for projects

--- sample_tracker/main.py
+++ sample_tracker/main.py
@@ -16,19 +16,20 @@ def main() -> None:
     create_tables()
 
     parser = argparse.ArgumentParser(description="Research Sample Tracker") #ArgumentParser is used to create a command-line interface (CLI) for the application
+
     subparsers = parser.add_subparsers(dest="command") #subparsers allow us to define different commands for the CLI
 
-    # Define add_project command
+    # Add command
     add_parser = subparsers.add_parser("add", help="Add a new research project") #add command is used to add a new research project
 
     add_parser.add_argument("name", type=str, help="Name of the research project") #name argument is used to specify the name of the research project
 
+    # List command
+    subparsers.add_parser("list", help="List all research projects") #list command is used to list all saved research projects
+
     args = parser.parse_args() #parse the command-line arguments
 
-    #args.command == "add"
-    #args.name == "Microbiome Study"
-
-    ###
+    # Handle add command
     if args.command == "add":
         project = create_project(args.name)
 
@@ -40,6 +41,14 @@ def main() -> None:
 
         print("Saved projects:")
 
+        for saved_project_id, saved_project_name in projects:
+            print(f"{saved_project_id}: {saved_project_name}")
+
+    # Handle list command
+    elif args.command == "list":
+        projects = list_projects()
+
+        print("Saved projects:")
         for saved_project_id, saved_project_name in projects:
             print(f"{saved_project_id}: {saved_project_name}")
 
